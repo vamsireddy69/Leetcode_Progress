12334725648ae6e30da2ef91c3b1668fc4870664
Vamsi Krishna Reddy Goluguri <Ivamsireddyu@gmail.com>
2026-06-14 23:18:53 +0530

    Time: 6 ms (26.08%), Space: 12.3 MB (91.32%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,34 @@
+class Solution:
+    def myAtoi(self, s):
+        i = 0
+        n = len(s)
+
+        # Skip leading spaces
+        while i < n and s[i] == ' ':
+            i += 1
+
+        # Sign
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        # Convert digits
+        num = 0
+        while i < n and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
+        # Clamp to 32-bit signed integer range
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if num < INT_MIN:
+            return INT_MIN
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
