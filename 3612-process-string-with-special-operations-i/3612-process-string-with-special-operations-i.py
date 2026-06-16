b2777864b0eb845be93c96578a8a900bad3a8999
Vamsi Krishna Reddy Goluguri <Ivamsireddyu@gmail.com>
2026-06-16 23:27:43 +0530

    Time: 27 ms (68.42%), Space: 25.7 MB (67.76%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -0,0 +1,20 @@
+#translated using AI
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = []
+        n = len(s)
+
+        for i in range(n):
+            ch = s[i]
+
+            if ch == '*':
+                if len(res) != 0:
+                    res.pop()
+            elif ch == '#':
+                res.extend(res)
+            elif ch == '%':
+                res.reverse()
+            elif 'a' <= ch <= 'z':
+                res.append(ch)
+
+        return ''.join(res)
