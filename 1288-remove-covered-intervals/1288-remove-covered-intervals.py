8dc4c156af1ce283dbded2e509f8d07a62cb5b1d
Vamsi Krishna Reddy Goluguri <Ivamsireddyu@gmail.com>
2026-07-06 23:56:51 +0530

    Time: 6 ms (29.78%), Space: 19.8 MB (13.54%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -0,0 +1,14 @@
+#translated using AI
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+
+        count = 0
+        max_end = 0
+
+        for start, end in intervals:
+            if end > max_end:
+                count += 1
+                max_end = end
+
+        return count
